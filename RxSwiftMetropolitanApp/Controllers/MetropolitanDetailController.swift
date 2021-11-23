97d1ee25931d9a44fbab0cfdd5ec5cbfb50bc848
Katopan <76249494+katomizuki@users.noreply.github.com>
2021-11-23 20:57:05 +0900

    slider setup

--- RxSwiftMetropolitanApp/Controllers/MetropolitanDetailController.swift
+++ RxSwiftMetropolitanApp/Controllers/MetropolitanDetailController.swift
@@ -1,50 +1,52 @@
-//
-//  MetropolitanDetailController.swift
-//  RxSwiftMetropolitanApp
-//
-//  Created by ミズキ on 2021/11/09.
-//
-
 import Foundation
 import UIKit
 import RxSwift
 import RxCocoa
 import SDWebImage
+import CoreImage
+
 class MetropolitanDetailController:UIViewController,Coordinating {
     var coordinator: Coordinator?
     var viewModel:ArtObjectViewModel?
     private let disposeBag = DisposeBag()
+    private let intensitySlider = UISlider()
+    private let radiusSlider = UISlider()
     private var imageView = UIImageView()
-    private var displayLabel = UILabel()
-    private var titleLabel = UILabel()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
-        displayLabel.numberOfLines = 0
-        titleLabel.numberOfLines = 0
-        displayLabel.sizeToFit()
-        titleLabel.sizeToFit()
-        view.addSubview(imageView)
-        view.addSubview(displayLabel)
-        view.addSubview(titleLabel)
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        displayLabel.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30).isActive = true
-        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        imageView.heightAnchor.constraint(equalToConstant: 100).isActive = true
-        imageView.widthAnchor.constraint(equalToConstant: 100).isActive = true
-        
-        titleLabel.topAnchor.constraint(equalTo: imageView.topAnchor, constant: 30).isActive = true
-        titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        titleLabel.heightAnchor.constraint(equalToConstant: 40).isActive = true
-        
-        displayLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30).isActive = true
-        displayLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        displayLabel.heightAnchor.constraint(equalToConstant: 40).isActive = true
-        setupUI()
+        setupBinding()
     }
     private func setupUI() {
+        view.backgroundColor = .white
+        let intensityLabel = UILabel()
+        let radiusLabel = UILabel()
+        intensityLabel.text = "強度"
+        intensityLabel.font = .systemFont(ofSize: 18)
+        intensityLabel.textColor = .darkGray
+        radiusLabel.text = "丸み"
+        radiusLabel.font = .systemFont(ofSize: 18)
+        radiusLabel.textColor = .darkGray
+        view.addSubview(imageView)
+        let stack = UIStackView(arrangedSubviews: [intensityLabel,intensitySlider,radiusLabel,radiusSlider])
+        stack.axis = .vertical
+        stack.distribution = .equalSpacing
+        stack.spacing = 10
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30).isActive = true
+        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: CGFloat(view.frame.width)).isActive = true
+        view.addSubview(stack)
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.topAnchor.constraint(equalTo: imageView.bottomAnchor,constant: 30).isActive = true
+        stack.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 30).isActive = true
+        stack.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -30).isActive = true
+        intensitySlider.addTarget(self, action: #selector(intensityChanged), for: .valueChanged)
+        radiusSlider.addTarget(self, action: #selector(radiusChanged), for: .valueChanged)
+    }
+    private func setupBinding() {
         guard let viewModel = viewModel else {
             return
         }
@@ -54,5 +56,11 @@ class MetropolitanDetailController:UIViewController,Coordinating {
                 self?.imageView.sd_setImage(with: url, completed: nil)
             }
             .disposed(by: disposeBag)
+    }
+    @objc private func intensityChanged() {
+        print(#function)
+    }
+    @objc private func radiusChanged() {
+        print(#function)
     }
 }
